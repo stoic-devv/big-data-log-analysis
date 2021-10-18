aa0c4558783dc6e5b375fa36d5a1d940b37abe35
stoic-devv <adityakanthale@gmail.com>
2021-10-17 20:12:35 -0500

    add config params and constants

---
+++ src/main/scala/constants/AppConstants.scala
@@ -0,0 +1,26 @@
+package constants
+
+
+object LogConstants extends Enumeration {
+  type String
+  val WARN = "WARN"
+  val INFO = "INFO"
+  val ERROR = "ERROR"
+  val DEBUG = "DEBUG"
+}
+
+object JobsConfigConstants extends Enumeration {
+  type String
+  val FILE_NAME = "jobs"
+  val OBJ_NAME = "jobs"
+  val BASE_OUTPUT_DIR = "BASE_OUTPUT_DIR"
+  val DISTRIBUTION_OUTPUT_DIR = "DISTRIBUTION_OUTPUT_DIR"
+  val MESSAGE_TYPE_OUTPUT_DIR = "MESSAGE_TYPE_OUTPUT_DIR"
+}
+
+object DistributionJobConstants extends Enumeration {
+  type String
+  val OBJ_NAME = "dist-job"
+  val INTERVAL = "INTERVAL"
+  val MAPRED_SEPARATOR_PARAM = "mapred.textoutputformat.separator"
+}
