f658bb7c488df9e7bab6014a38f088231a2bbc4d
stoic-devv <adityakanthale@gmail.com>
2021-10-17 20:16:04 -0500

    try out new apis

--- src/main/scala/mapreduce/WordCount.scala
+++ src/main/scala/mapreduce/WordCount.scala
@@ -1,10 +1,13 @@
 package mapreduce
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.{IntWritable, LongWritable, Text}
-import org.apache.hadoop.mapred.{FileInputFormat, FileOutputFormat, JobClient, JobConf, MapReduceBase, Mapper, OutputCollector, Reducer, Reporter, SequenceFileOutputFormat, TextInputFormat, TextOutputFormat}
-import org.apache.hadoop.mapreduce.lib.output
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import org.apache.hadoop.mapreduce.{Job, Mapper, Reducer}
 
+import java.io.IOException
 import java.util.{Iterator, StringTokenizer}
 import scala.annotation.tailrec
 
@@ -12,64 +15,52 @@ class WordCount
 
 object WordCount:
 
-  class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable] {
+  @throws(classOf[IOException])
+  @throws(classOf[InterruptedException])
+  class Map extends Mapper[Object, Text, Text, IntWritable] {
     val one = new IntWritable(1)
     val word = new Text()
 
-    override def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit = {
-      val itr = new StringTokenizer(value.toString)
-
-      /*@tailrec def collect(itr: StringTokenizer): Unit = itr.hasMoreTokens() match {
-        case true => {
-          word.set(itr.nextToken())
-          output.collect(word, one)
-        }
-      }
-
-      collect(new StringTokenizer(value.toString))*/
-
+    override def map(key: Object, value: Text, context: Context): Unit = {
+      val itr = new StringTokenizer(value.toString())
 
       while (itr.hasMoreTokens()) {
         word.set(itr.nextToken())
-        output.collect(word, one)
+        context.write(word, one)
       }
     }
   }
 
-  class Reduce extends MapReduceBase with Reducer[Text,IntWritable,Text,IntWritable] {
+  @throws(classOf[IOException])
+  @throws(classOf[InterruptedException])
+  class Reduce extends Reducer[Text, IntWritable, Text, IntWritable] {
 
+    val result = new IntWritable()
     def reduce(key: Text, values: Iterator[IntWritable],
-               output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit = {
+               context: Context): Unit = {
       var sum = 0
       while (values.hasNext) {
         sum += values.next().get()
       }
-      output.collect(key, new IntWritable(sum))
+      result.set(sum)
+      context.write(key, result)
     }
 
   }
 
-  /*def main(args: Array[String]): Unit = {
-    val conf = new JobConf(this.getClass)
-    conf.setJobName("my word count")
-
-    conf.setJarByClass(this.getClass)
-
-    conf.setOutputKeyClass(classOf[Text])
-    conf.setOutputValueClass(classOf[IntWritable])
-
-    conf.setMapperClass(classOf[Map])
-    conf.setCombinerClass(classOf[Reduce])
-    conf.setReducerClass(classOf[Reduce])
-
-    conf.setInputFormat(classOf[TextInputFormat])
-    conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
-
-    FileInputFormat.setInputPaths(conf, new Path(args(0)))
-    FileOutputFormat.setOutputPath(conf, new Path(args(1)))
-
-    JobClient.runJob(conf)
-
+/*  @throws(classOf[Exception])
+  def main(args: Array[String]): Unit = {
+    val conf = new Configuration()
+    val job = Job.getInstance(conf, "word count")
+    job.setJarByClass(classOf[WordCount])
+    job.setMapperClass(classOf[Map])
+    //job.setCombinerClass(classOf[Reduce])
+    job.setReducerClass(classOf[Reduce])
+    job.setOutputKeyClass(classOf[Text])
+    job.setOutputValueClass(classOf[IntWritable])
+    FileInputFormat.addInputPath(job, new Path(args(0)))
+    FileOutputFormat.setOutputPath(job, new Path(args(1)))
+    job.waitForCompletion(true)
   }*/
   
   
