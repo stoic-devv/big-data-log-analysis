1addbce7916a2ba31f9e5fbeafc69830e0cfa649
stoic-devv <adityakanthale@gmail.com>
2021-10-17 20:15:01 -0500

    remove unused code

--- src/main/scala/utils/LogReader.scala
+++ src/main/scala/utils/LogReader.scala
@@ -1,10 +1,7 @@
 package utils
 
-import entity.LogEntity
+import entity.{LogEntity}
 
-import java.util
-import java.util.Iterator
-import scala.io.Source
 
 class LogReader
 
@@ -41,27 +38,3 @@ object LogReader:
         words(logConf.getInt("logtypeidx")), words(logConf.getInt("errordebugidx")))
     }
 
-  /*def readFile(fname: String): Unit =
-    for (line <- Source.fromFile(fname).getLines) {
-      println(parseLogStr(line))
-    }*/
-
-  /*def main(args: Array[String]): Unit =
-
-    val u = new util.ArrayList[Int]()
-    u.add(1)
-    u.add(2)
-    u.add(3)
-
-    def calcSum(accum: Int, itr: Iterator[Int]): Int = {
-      itr.hasNext match {
-        case true => calcSum(accum + itr.next(), itr)
-        case false => accum
-      }
-    }
-
-    println(calcSum(0, u.iterator()))
-
-
-    //readFile("C:\\Users\\achand50\\Downloads\\cs441\\big-data-log-analysis\\src\\main\\scala\\utils\\sample.log")
-*/
